2c14b792399bba05a26d8c45fa0c1ea17718cfce
logan-hartford <91397886+logan-hartford@users.noreply.github.com>
2024-11-15 11:13:00 -0500

    sheild stuff added and draw diagram finished

--- arduino/rgb_sensor/rgb_sensor.ino
+++ arduino/rgb_sensor/rgb_sensor.ino
@@ -1,4 +1,6 @@
 #include <Servo.h>
+#include <Ethernet.h>
+#include <ArduinoMqttClient.h>
 
 #define S0 7
 #define S1 5
@@ -19,7 +21,25 @@
 #define GREEN_POS 120
 #define YELLOW_POS 180
 
+#define ETHERNET_SHEILD false
+
 Servo servo;
+
+//Information about the Ethernet Shield:
+//The mac address printed on the bottom of your Ethernet Shield
+byte mac[] = {  0xA8, 0x61, 0x0A, 0xAF, 0x00, 0xD0 };//example: { 0xA7, 0x61, 0x0A, 0xAD, 0x00, 0x2B }
+
+// Assign a unique IP address for your client, the Arduino (note that the octets are separated by commas, not points)
+IPAddress ip( 192,168,0,242); //example: ip(192,168,0,242)
+
+//MQTT broker information (Same IP as used for the TIA V18 MqttDB broker)
+IPAddress broker(192,168,0,241); //example: ip(192,168,0,241)
+int port     = 1883;
+
+const char topic[]  = "motor";
+
+EthernetClient client;
+MqttClient mqttClient(client);
 
 unsigned int red;
 unsigned int green;
@@ -61,12 +81,56 @@ void setup() {
   // Set LEDs
   digitalWrite(LED_PIN, LED_ON);
   setRGBLED("off");
-  
+
   // Set servo to default postition
   servo.write(0);
+
+  if (ETHERNET_SHEILD)
+  {
+    Ethernet.begin(mac, ip);
+    delay(1000); // Give the Ethernet shield a second to initialize
+    // Check for Ethernet hardware present
+    if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+      Serial.println("Ethernet shield was not found.  Sorry, can't run without hardware. :(");
+      while (true) {
+        delay(1); // Do nothing, no point running without Ethernet hardware
+      }
+    }
+    if (Ethernet.linkStatus() == LinkOFF) {
+      Serial.println("Ethernet cable is not connected.");
+    }
+
+    // Give the Ethernet shield a second to initialize
+    delay(1000);
+    Serial.println("Attempting to connect to the designated IP...");
+
+    // Connect to the server
+    if (client.connect(broker,1883)) {
+      Serial.println("Connected to IP");
+      // Make a HTTP request
+      
+    } else {
+      Serial.println("Connection failed");
+    }
+
+    //Connect to the MQTT broker
+    Serial.print("Attempting to connect to the MQTT broker: ");
+    Serial.println(broker);
+
+    while (!mqttClient.connect(broker, port)) {
+      Serial.print("MQTT connection failed! Error code = ");
+      Serial.println(mqttClient.connectError());
+    }
+    Serial.println("You're connected to the MQTT broker!");
+    Serial.println();
+  }
 }
 
 void loop() {
+  if (ETHERNET_SHEILD)
+  {
+    mqttClient.poll();
+  }
   // Read color sensor
   getColors(0.2);
 
@@ -157,6 +221,13 @@ void turnMotorOn(bool on)
   else
   {
     setRGBLED("purple");
+  }
+
+  if (ETHERNET_SHEILD)
+  {
+    mqttClient.beginMessage(topic);
+    mqttClient.print(on);
+    mqttClient.endMessage();
   }
 }
 
